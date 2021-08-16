1052096cbdcefee40e2d71dd9123c4d6d53ead71
Greedysky <greedysky@163.com>
2021-08-16 22:59:48 +0800

    2021-08-16 22:55:40

--- ttkdocs/html/search-config.php
+++ ttkdocs/html/search-config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun Jun 27 2021 13:29:13 for TTKMusicplayer by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.8 ",
+  'logo' => "Generated on Mon Aug 16 2021 22:53:14 for TTKMusicplayer by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.8 ",
 );
 
 </script>
