c1718179506980eedc2e7ad6a0051df2c5322238
Greedysky <greedysky@163.com>
2022-03-05 14:57:38 +0800

    2022-03-05 14:54:36

--- ttkdocs/html/search-config.php
+++ ttkdocs/html/search-config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun Jan 2 2022 14:30:33 for TTKMusicplayer by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.8 ",
+  'logo' => "Generated on Sat Mar 5 2022 14:46:23 for TTKMusicplayer by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.8 ",
 );
 
 </script>
