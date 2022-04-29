4db759385a0ddd671afa1b0ae40f696c6a9bd337
Greedysky <greedysky@163.com>
2022-04-30 00:42:29 +0800

    2022-04-30 00:39:32

--- ttkdocs/html/search-config.php
+++ ttkdocs/html/search-config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sat Mar 5 2022 14:46:23 for TTKMusicplayer by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.8 ",
+  'logo' => "Generated on Sat Apr 30 2022 00:32:07 for TTKMusicplayer by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.8 ",
 );
 
 </script>
